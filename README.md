35d7e6076a866d6495643fc2d6e54bc7f99f0d53
Carl <morrios@163.com>
2019-08-08 15:10:14 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Morrios Sms
---
+++ src/Common.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Morrios\Sms;
+
+
+/**
+ * Class Common
+ * @package Morrios\Sms
+ */
+class Common
+{
+    /**
+     * test
+     * @author lvshuai
+     */
+    public function test()
+    {
+        echo 'test output...';
+    }
+}
